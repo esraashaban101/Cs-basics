a9c35c61365abc6c89ddb72700c615cf14b18710
amirsakrGH <amirsakr001@gmail.com>
2024-12-13 10:12:40 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question1
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num);
+            #endregion
+
+
+            #region Question2
+            //int firstnum = 5, secondnum = 10;
+            //firstnum = secondnum;
+            //firstnum++;
+            //Console.WriteLine(firstnum);
+            //Console.WriteLine(secondnum);
+            /*first num will be 11 and second num will stay 10
+             * because two values are value type
+             */
+            #endregion
+
+
+            #region Question3
+            //Class1 C1 = new Class1();
+            //C1.x = 10;
+            //Class1 C2 = new Class1();
+            //C2.x = 1;
+            //C2 = C1;
+            //C1.x = 20;
+            //Console.WriteLine(C1.x);
+            //Console.WriteLine(C2.x);
+            /* C2.x and C1.x both nums will be 20 
+            * because two values are references type so they pointing to the same address
+            */
+            #endregion
+        }
+    }
+}
